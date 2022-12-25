67524276f1c72a6d3ee0306eea32ea920bc7d905
MohamedShakshak <108498735+MohamedShakshak@users.noreply.github.com>
2022-12-25 05:18:58 +0200

    Add files via upload

--- Database/Hospital.sql
+++ Database/Hospital.sql
@@ -1,80 +1,16 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
---
--- Host: localhost    Database: hospital
--- ------------------------------------------------------
--- Server version	8.0.31
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `patient`
---
-
-DROP TABLE IF EXISTS `patient`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `patient` (
-  `patientno` varchar(50) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `phone` int DEFAULT NULL,
-  `address` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`patientno`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `patient`
---
-
-LOCK TABLES `patient` WRITE;
-/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient` VALUES ('PS001','mohamed',155165,'hgnnghgh');
-/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `username` varchar(200) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `uType` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'mm','mohamed','446629','Doctor'),(2,'Mohamed','mohamed20','1234','Pharmacist');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-12-18  2:15:03
+#create database Hospital;
+#create table User ( int primary key auto_increment,name varchar(50) not null,username varchar(200) not null,password varchar(50) not null,gender varchar(30));
+#alter table user drop column gender;
+#alter table User add column (uType varchar(50) not null); 
+#delete  from  user where id=2 and id =3;
+#delete from User where id=3;
+#truncate table user;
+#select * from user  
+#delete from User where id=4;
+#create table Patient (patientno varchar(50) primary key , name varchar(150) not null,phone int,  varchar(300));
+#update patient set name='Omar',phone=12531,address='Qalag' where patientno='PS002';
+#create table Doctor(DoctorNum varchar(100) primary key,name varchar(150) not null,Special varchar(100) not null,Cost int ,phone int(11),room int,logid int references User(id));
+#update Doctor set DoctorNum=?, name=?,Special=?,Cost=?,phone=?,room=?  where DoctorNum=?;
+#update Doctor set name='Omar' , Special='mm' , Cost=151 , phone=1551156 , room= 5  where DoctorNum='DS001';
+#create table Medicine (medid varchar(50) primary key,medname varchar(100) not null,description varchar(250),sellPrice int,buyPrice int ,qun int );
+select *from  Medicine;
